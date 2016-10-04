68d5e0d3152fb9d9801bcb22ed084708d21f8774
Dariusz Borcz <borcz.dariusz@gmail.com>
2016-10-04 22:34:47 +0200

    Zadanie 8.7

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var womensNames = ["Asia", "Kasia", "Ola", "Jola"];
+var mensNames = ["Piotrek", "Marek", "Arek", "Jarek"];
+var allNames = womensNames.concat(mensNames);
+
+console.log("Imiona damskie: " + womensNames);
+console.log("Imiona męskie: " + mensNames);
+console.log("Wszystkie imiona: " + allNames);
+
+var newName = "Marian";
+
+if (allNames.indexOf("Marian") == -1) {
+
+allNames.push("Marian");
+console.log("Lista z nowym imieniem:  " + allNames);
+
+} else {
+	console.log("Imię znajduje się już na liście");
+}
