09cbbbb42a7b3685754ba670fb8d055034f7f76e
Dariusz Borcz <borcz.dariusz@gmail.com>
2016-10-08 20:38:06 +0200

    dodanie li jako ost. el. zmiennej 'lista', za pomocą createElement oraz appendChild

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ var lista = document.getElementById('list');
 var addNewLi = document.getElementById('addElem');
 
 addNewLi.addEventListener('click', function(e) { 
+
 	var itemsByTagName = document.getElementsByTagName('li');
 	var howManyLi = itemsByTagName.length;
-	lista.innerHTML += '<li>item</li>' + howManyLi; 
+	var li = document.createElement('li');
+	li.innerHTML += 'item ' + howManyLi; 
+	lista.appendChild(li);
 });
